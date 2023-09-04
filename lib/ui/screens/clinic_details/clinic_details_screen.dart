5b1626bc55fdb085ad6b546b655ad9a6e47d293b
muhammed-sayouh <m_9abdo@yahoo.com>
2023-09-04 21:10:52 +0200

    4/9

---
+++ lib/ui/screens/clinic_details/clinic_details_screen.dart
@@ -0,0 +1,15 @@
+import 'package:medcloud/helper/import_helper.dart';
+
+class ClinicDetailsScreen extends StatelessWidget {
+  const ClinicDetailsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScreen(
+          child: Column(
+        children: [],
+      )),
+    );
+  }
+}
